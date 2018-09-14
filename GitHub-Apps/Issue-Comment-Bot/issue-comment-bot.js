7a5685dcb32a1838426097427251e3505f7e69c6
anjakammer <post@anjakammer.de>
2018-09-14 17:49:31 +0200

    code formatting

--- GitHub-Apps/Issue-Comment-Bot/issue-comment-bot.js
+++ GitHub-Apps/Issue-Comment-Bot/issue-comment-bot.js
@@ -1,32 +1,30 @@
 const requestp = require('request-promise')
 const jwt = require('jsonwebtoken')
 
-exports.autoComment = (req, res) => {
-
-  const cert = process.env.PRIVATE_KEY.replace(/###n/g, '\n');
+exports.main = (req, res) => {
+  const cert = process.env.PRIVATE_KEY.replace(/###n/g, '\n')
 
   const webToken = jwt.sign({
-      iss: process.env.APP_ID
-    },
-    cert, {
-      algorithm: 'RS256',
-      expiresIn: '10m'
-    })
+    iss: process.env.APP_ID
+  },
+  cert, {
+    algorithm: 'RS256',
+    expiresIn: '10m'
+  })
 
-  const addComment = (url, body, token) =>
-    requestp({
-      json: true,
-      headers: {
-        'Authorization': 'token ' + token,
-        'User-Agent': process.env.APP_NAME,
-        'Accept': 'application/vnd.github.machine-man-preview+json'
-      },
-      method: 'POST',
-      url,
-      body: {
-        body
-      }
-    })
+  const addComment = (url, body, token) => requestp({
+    url,
+    json: true,
+    headers: {
+      'Authorization': 'token ' + token,
+      'User-Agent': process.env.APP_NAME,
+      'Accept': 'application/vnd.github.machine-man-preview+json'
+    },
+    method: 'POST',
+    body: {
+      body
+    }
+  })
 
   const installationToken = (installationId) => requestp({
     url: `https://api.github.com/installations/${installationId}/access_tokens`,
@@ -43,7 +41,6 @@ exports.autoComment = (req, res) => {
   if (req.body === undefined) {
     res.status(400).send('No message defined!')
   } else {
-    res.status(200).send('OK');
     const payload = req.body
     if (payload.action === 'opened') {
       installationToken(payload.installation.id)
@@ -51,7 +48,8 @@ exports.autoComment = (req, res) => {
           token
         }) => {
           const comment = 'yeah baby!'
-          return addComment(payload.issue.comments_url, comment, token)
+          addComment(payload.issue.comments_url, comment, token)
+          return res.status(200).send('OK')
         })
     }
   }
